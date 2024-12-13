0426547d0280f2da459bf9096fd11536d2dfe5aa
Speedle <mr.speedle@yahoo.cl>
2024-12-13 19:43:19 -0300

    v0.2 routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,18 @@
 Rails.application.routes.draw do
-  get "deliveries/index"
-  get "deliveries/new"
-  get "deliveries/create"
-  get "materials/index"
-  get "materials/show"
-  get "materials/new"
-  get "materials/create"
-  get "materials/edit"
-  get "materials/update"
-  get "materials/destroy"
-  get "recyclers/index"
-  get "recyclers/show"
-  get "recyclers/new"
-  get "recyclers/create"
-  get "recyclers/edit"
-  get "recyclers/update"
-  get "recyclers/destroy"
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  resources :recyclers do
+    resources :materials, only: [:index, :new, :create]
+    member do
+      get :deliveries
+    end
+  end
+
+  resources :deliveries, only: [:new, :create, :index]
+
+  root "recyclers#index"
+
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
